c60075a0869a02dfa6083a49c4ff4bb739371a8e
gibbyt <gilbertotovar2021@gmail.com>
2022-07-01 14:47:14 -0500

    Still some bugs when trying to mod the og dataframe

--- test.py
+++ test.py
@@ -1,14 +1,11 @@
+from cProfile import label
 import pandas as pd
 import numpy as np
+import openpyxl
 
 
-
-LABEL = ["NAM", "EMEA", "LATAM", "APAC", "OTHER"]
-CAN_LIST = ["ON", "QC", "NS", "NB", "MB", "BC", "PE", "SK", "AB", "NL"]
-
-
-# Getting to know how this git thing works
-def loc_label (location):
+def loc_label(location):
+    dict = pd.read_excel(r"C:\Users\gilberto.tovar\test\Countries and Regions.xlsx")
 
     split = location.split(',')
     if len(split) == 1:
@@ -16,23 +13,32 @@ def loc_label (location):
     else:
         city = split[0]
         country = split[1]
+    
+    #Inferecnce that all acronyms are either the UK or in NA
+    if len(country) < 4:
+        if "UK" in country:
+            return "EMEA"
+        return "NA"
 
-    #Make it into country
-    if len(country) < 3:
-        if country == "UK":
-            return LABEL[1]
-        return LABEL[0]
-        # check where it belongs
-        
     #check in dictionary
-
+    for i in range(len(dict)):
+        if dict.loc[i, "Country"] in country:
+            return dict.loc[i, "Region"]
     #return the label
     return country
 
 
+df = pd.read_csv(r'C:\Users\gilberto.tovar\test\data copy.csv')
 
-data = pd.read_csv(r'C:\Users\gilberto.tovar\test\data.csv')
-df = pd.DataFrame(data, columns= ['Location'])
-print(df.loc[1])
+list = []
+for i in range(len(df)):
+    label = loc_label(df.loc[i, "Location"])
+    list.append(label)
+df["Region"] = list
+
+
+
+
+
 
 
